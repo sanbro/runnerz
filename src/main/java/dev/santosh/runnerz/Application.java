c7bfff2f6666e8c97b7e66d2f7c9e6206ca7e1ba
Santosh Sahani <sanbro18@gmail.com>
2024-04-05 00:04:00 +0400

    All file initialized

---
+++ src/main/java/dev/santosh/runnerz/Application.java
@@ -0,0 +1,33 @@
+package dev.santosh.runnerz;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.santosh.runnerz.run.Location;
+import dev.santosh.runnerz.run.Run;
+
+@SpringBootApplication
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		// log.info("Application Running Successfully");
+	}
+
+	@Bean
+	CommandLineRunner runner(){ 
+		return args -> {
+			Run run = new Run(1, "First Run",LocalDateTime.now(),LocalDateTime.now().plus(1,ChronoUnit.HOURS), 5, Location.INDOOR);
+			log.info("Run:" + run);
+		};
+	} 
+
+}
---
+++ src/main/java/dev/santosh/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package dev.santosh.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
